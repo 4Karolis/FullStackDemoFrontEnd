86edbeb7cdb036d8df422cde8a12b554122a4f8c
Karolis Tarasevičius <100716265+4Karolis@users.noreply.github.com>
2022-10-10 19:50:53 +0300

    Add files via upload

---
+++ FrontEnd/js/index.js
@@ -0,0 +1,28 @@
+document.querySelector(`#login`).addEventListener(`submit`, (event) =>{
+    event.preventDefault();
+
+    console.log(`Login submit`);
+
+    const loginRequest = {
+        userName: event.target.name.value,
+        password: event.target.password.value
+    };
+    fetch(`https://localhost:7275/api/Auth/Login`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(loginRequest)
+    })
+    .then(response => response.json())//sekmes atveju 
+    .then(result => {
+        if(result.errorMessage){
+            alert(result.errorMessage);
+            return;
+        }else{
+            sessionStorage.setItem('token', result.token);
+            document.location.href = '/weather.html'
+        }
+    })
+    .catch((error) => alert(error));
+});
---
+++ FrontEnd/js/weather.js
@@ -0,0 +1,24 @@
+const token = sessionStorage.getItem('token');
+
+if(!token){
+    alert('please login in order to see weather forecast!');
+    document.location.href = '/index.html';
+} else {
+    fetch('https://localhost:7275/WeatherForecast', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token
+            // arba 'Authorization' : `Bearer ${token}`
+        }
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        const container = document.querySelector(`#weatherContainer`);
+        for(let i = 0; i < response.length; i++){
+            const weatherParagraph = document.createElement('p');
+            weatherParagraph.innerHTML = `${response[i].date} </b> temperature: <b>${response[i].temperatureC} C , weatherr will be ${response[i].summary}</b>`
+            container.append(weatherParagraph);
+        }
+    })
+    .catch((error) => alert(error));
+}
